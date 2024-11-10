0321411a98ad6e0403a2b18efe6d1284113bcebb
llghtdark <lcfilho19@gmail.com>
2024-11-10 20:35:08 -0300

    some bug fixes

--- main.js
+++ main.js
@@ -19,19 +19,22 @@ function Connect(keypressed){
 function editLink(buttonID){
     let newLink = prompt("Enter a new URL for " + buttonID + " (Let it clear to reset)");
     let button = document.getElementById(buttonID);
-    if (newLink) {
+    console.log(newLink); 
+    if (newLink === "") {
+        // User cleared the link (or left the prompt empty)
+        button.setAttribute("data-link", "");
+        button.setAttribute("title", "");
+        button.style.backgroundColor = "";
+        button.style.color = "#f1ede7";
+        localStorage.removeItem(buttonID);
+        console.log("cleared");
+    } else if (newLink) {
         button.setAttribute("data-link", newLink);
         button.setAttribute("title", newLink); // Shows the link on hover
         button.style.backgroundColor = "#f38aff";
         button.style.color = "#141414";
         localStorage.setItem(buttonID, newLink);  // Save in localStorage
-    }else {
-        // User cleared the link (or left the prompt empty)
-        button.setAttribute("data-link", "");         // Keep the attribute but empty its content
-        button.setAttribute("title", "");
-        button.style.backgroundColor = "";
-        button.style.color = "#f1ede7";
-        localStorage.removeItem(buttonID);            // Remove from localStorage
+        console.log("added");
     }
 }
 
